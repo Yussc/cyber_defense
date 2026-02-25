9dc08850eba4183a192f554c76c48fae1adf0543
Yussc <yassine.chiadmi3@gmail.com>
2026-02-25 03:18:37 +0100

    update

--- router/setup-iptables.sh
+++ router/setup-iptables.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# 1. On vide les règles existantes
+# vide des règles existantes
 iptables -F
 iptables -t nat -F
 
-# 2. Politique par défaut : On bloque tout (Zero Trust)
+# Zero Trust
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 
-# 3. Autoriser le trafic déjà établi (pour ne pas couper les connexions en cours)
 iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# 4. Autoriser le HTTP (80) et HTTPS (443) vers le serveur Web
 iptables -A FORWARD -p tcp -d 192.168.100.10 --dport 80 -j ACCEPT
 iptables -A FORWARD -p tcp -d 192.168.100.10 --dport 443 -j ACCEPT
 
-# 5. Autoriser Wazuh (1514, 1515) dans le LAN
+# Autoriser Wazuh (1514, 1515) dans le LAN
 iptables -A FORWARD -p tcp -d 192.168.100.30 --dport 1514:1515 -j ACCEPT
 
-# 6. Masquerading (NAT) pour que le LAN puisse sortir via le WAN
+# Masquerading (NAT) pour que le LAN puisse sortir via le WAN
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
